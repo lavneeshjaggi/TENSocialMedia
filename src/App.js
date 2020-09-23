c3ec1b00e5230146cb817ebb309effbc41131998
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-09-24 00:01:00 +0530

    Make the initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Homepage from "./pages/homepage/homepage.component";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./homepage.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <h1>Social Media Website Front Page Is Currently Under Development</h1>
+  </div>
+);
+
+export default Homepage;
